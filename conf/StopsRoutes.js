012ddfd054e8029dc18c2ba529d1eeaeb46f962e
fabio <fmts@ua.pt>
2019-10-25 11:38:21 +0100

    alteracoes

--- conf/StopsRoutes.js
+++ conf/StopsRoutes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var AgenciesController = require('../controllers/Agencies')
+var StopsController = require('../controllers/Stops')
 
-router.get('/agency', AgenciesController.getAgencies)
-router.get('/agency/:agencyKey', AgenciesController.getAgencyById)
+router.get('/stop', StopsController.getStops)
+router.get('/stop/:stopId', StopsController.getStopById)
+router.get('/stoptimes/:stopId', StopsController.getStoptimes)
 
 module.exports = router;
